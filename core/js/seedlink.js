ec47f69a9e1483f80b2447fb7bfd19e10c24d2f7
Akbar Yahya <akbaryahya_ym@yahoo.com>
2020-06-06 13:42:13 +0800

    step sync part 1

--- core/js/seedlink.js
+++ core/js/seedlink.js
@@ -23,13 +23,18 @@ seedlink.onmessage = function (eventt) {
 }
 
 //Get Index Time
-function getDates(startDate, stopDate) {
+function getDates(startDate, stopDate, sampel=0) {
     var dateArray = new Array();
     var currentDate = startDate;
+
+    if(sampel !== 0){
+        currentDate = Math.floor(currentDate / sampel);
+        stopDate = Math.floor(stopDate / sampel);
+    }
+
     while (currentDate <= stopDate) {
         dateArray.push(currentDate);
-        currentDate++;
-        //console.log(currentDate);
+        currentDate++;        
     }
     return dateArray;
 }
@@ -51,12 +56,12 @@ function Station(data, put = "auto") {
 
     //Data RAW
     var data_sampel = [];
-    var index_time = getDates(start, end);
+    var index_time = getDates(start, end,1000);
     sampel.forEach((val, index) => {
         data_sampel.push({
             x: index_time[index],
             y: val
-        })
+        });
     });
 
     //Peak ground acceleration (PGA) sama dengan percepatan tanah maksimum yang terjadi selama gempa bumi di suatu lokasi. PGA sama dengan amplitudo percepatan absolut terbesar yang tercatat pada accelerogram di suatu lokasi saat terjadi gempa bumi tertentu
@@ -76,8 +81,8 @@ function Station(data, put = "auto") {
         console.log('tidak ada gempa');
     }
 
-    var infobody = 
-    ('\
+    var infobody =
+        ('\
         Time: ' + time + ' LocalTime <br>\
         PGA: ' + info_pga + ' <br>\
         Delayed: ' + delayed + ' sec (<time data-now="' + start + '" data-type="sec"></time>) <br> \
@@ -90,18 +95,18 @@ function Station(data, put = "auto") {
 
             //buat dulu
             out.insertAdjacentHTML('beforeend',
-            '<div class="modal-content mb-3" id="' + id + '">\
+                '<div class="modal-content mb-3" id="' + id + '">\
              <div class="modal-header">\
              <h5 class="modal-title" id="judul">' + id + '</h5>\
              </div>\
-             <div class="modal-body" id="body"><div id="subbody">'+infobody+'</div><div id="chart"></div></div>\
+             <div class="modal-body" id="body"><div id="subbody">' + infobody + '</div><div id="chart"></div></div>\
              </div>\
             ');
 
             //lalu input data chart
             var chart = new ApexCharts(document.getElementById(id).querySelector('#chart'), {
                 series: [{
-                    data: data_sampel
+                    data: []
                 }],
                 chart: {
                     id: 'realtime',
@@ -136,7 +141,8 @@ function Station(data, put = "auto") {
             station.push({
                 id: id,
                 chart: chart,
-                collection: [collection]
+                collection: [collection],
+                sampel: data_sampel
             })
 
         } else {
@@ -151,9 +157,12 @@ function Station(data, put = "auto") {
         for (var j in station) {
             if (station[j].id == id) {
 
+                /*
                 station[j].chart.updateSeries([{
                     data: data_sampel
                 }]);
+                */
+                station[j].sampel = data_sampel;
                 if (collection.length !== 0) {
                     station[j].collection.push(collection); //jika ada info push it
                 }
@@ -166,4 +175,58 @@ function Station(data, put = "auto") {
     } else {
         console.log('No TAG STATION');
     }
+}
+
+//sync both stations so that they can pick up in real time later and maybe we can analyze data directly here
+function sync() {
+    console.log('sync time....');
+
+    var sampel_tmp = [];
+
+    var tnow = new Date().getTime();
+    var index_time = getDates(tnow - 2 * 60 * 1000, tnow + 2 * 60 * 1000,1000);
+    console.log("Index Time: ",index_time.length);
+
+    //getRndInteger(-1000, 1000)
+    index_time.forEach((val, index) => {
+        sampel_tmp.push({
+            x: val,
+            y: 0
+        })
+    });
+    index_time = null;
+
+    station.forEach((data) => {
+
+        var sampel = data.sampel;
+        //ini sampel dari raw
+        for (var sr in sampel) {
+            //ini sampel tmp buat cari blok dari sampel real
+            for (var jt in sampel_tmp) {
+
+                //console.log(sampel[sr].x+" | "+sampel_tmp[jt].x);
+
+                if (sampel[sr].x == sampel_tmp[jt].x) {
+                    console.log('ada');                
+                    sampel_tmp[jt].y = sampel[sr].y;
+                    break;
+                }
+            };
+            //debugger;
+        };
+
+        //jika tmp sampel sudah di update, update it
+        var tb = data.chart;
+        tb.updateSeries([{
+            data: sampel_tmp
+        }]);
+    });
+
+    sampel_tmp = null;
+
+}
+setInterval(sync, 1000 * 5);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
