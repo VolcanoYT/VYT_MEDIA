62a90e71d02f070c6c8e242b300ae5c9da6ab1c1
Akbar Yahya <akbaryahya_ym@yahoo.com>
2020-06-07 03:54:52 +0800

    NICE

--- core/js/seedlink.js
+++ core/js/seedlink.js
@@ -2,21 +2,15 @@
 var event = [];
 var station = [];
 
-//higher less accurate because less data sampel but faster processing.
-var sampelrate = 1000;
+// higher less accurate because less data sampel but faster processing and does not take much memory
+var TMP_Sampel_Rate = 1000;
 
-//every 5 seconds synchronizes all stations.
-var sync_data = 5;
+// every 3 seconds synchronizes all stations.
+var delayed_sync_data = 3;
 
-// false when you are ready, gui is very useful when you are still debugging the process but it is quite slow for analysis data.
+// false when you are ready, gui is very useful when you are still debugging process but it is quite slow for analysis data.
 var gui = true;
 var gui_div = "auto";
-
-/*
-var mergeAll = function () {
-    return Array.prototype.slice.call(arguments).reduce(mergeSorted);
-};
-*/
 
 var seedlink = new WebSocket("wss://seedlink.volcanoyt.com");
 seedlink.onopen = function (event) {
@@ -64,11 +58,11 @@ function Station(data) {
     var start = data.start;
     var end = data.end;
     var sampel = data.data;
-    var sampleRate = data.sampleRate;
+    var STA_SampleRate = data.sampleRate;
 
     //Data RAW
     var data_sampel = [];
-    var index_time = getDates(start, end, sampelrate);
+    var index_time = getDates(start, end, TMP_Sampel_Rate);
     sampel.forEach((val, index) => {
         data_sampel.push({
             x: index_time[index],
@@ -83,7 +77,7 @@ function Station(data) {
             new_station = false;
 
             station[j].sampel = station[j].sampel.concat(...data_sampel);
-            console.log("sample collected: ", station[j].sampel.length);
+            //console.log("sample collected: ", station[j].sampel.length);
             station[j].end = end; //TODO: update "end" dengan data last sampel ketika di hapus
 
             break;
@@ -98,30 +92,35 @@ function Station(data) {
             sampel: data_sampel,
             start: start,
             end: end,
-            sampleRate: sampleRate
+            sampleRate: STA_SampleRate
         });
     }
 }
 
 //sync both stations so that they can pick up in real time later and maybe we can analyze data directly here
 function sync() {
-    //console.log('sync time....');
-
     var sampel_tmp = [];
 
     var tnow = new Date().getTime();
+    //var t_w_s = Math.floor(tnow / TMP_Sampel_Rate);
+
+    //line
     var go_start = tnow - 4 * 60 * 1000;
-    var go_center = Math.floor(go_start / sampelrate);
+    var go_center = Math.floor(go_start / TMP_Sampel_Rate);
     var go_end = tnow + 1 * 10 * 1000;
 
-    var index_time = getDates(go_start, go_end, sampelrate);
-    //console.log("Index Time: ",index_time.length);
+    //pick up
+    var go_select_start = Math.floor(go_start / TMP_Sampel_Rate) + 240;
+    var go_select_end = Math.floor(go_end / TMP_Sampel_Rate) - 20;
 
-    //getRndInteger(-1000, 1000)
+    //console.log("pick up: " + (go_select_start - go_select_end));
+
+    // index tmp
+    var index_time = getDates(go_start, go_end, TMP_Sampel_Rate);
     index_time.forEach((val, index) => {
         sampel_tmp.push({
             x: val,
-            y: 0
+            y: 0 //getRndInteger(-1000, 1000) TODO: coba nilai rata-rata
         })
     });
     index_time = null;
@@ -132,7 +131,6 @@ function sync() {
 
         //sebelum mulai cek dulu data lama
         var newupdate = data.sampel.filter(function (item) {
-            //console.log(item.x - go_center);
             return go_center <= item.x
         })
         station[sta].sampel = newupdate;
@@ -164,23 +162,36 @@ function sync() {
         var delayed = Math.floor(timenow / 1000) - Math.floor(data.end / 1000);
         var time = '' + moment(data.start).format('DD/MM/YYYY HH:mm:ss') + ' - ' + moment(data.end).format('DD/MM/YYYY HH:mm:ss') + ' (' + moment(timenow).format('DD/MM/YYYY HH:mm:ss') + ')';
 
-        //Data Select for pick up
+        //Data Select
         var data_select = [];
         newupdate.forEach((val) => {
-            data_select.push(val.y);
+            //Only for pick up         
+            if (go_select_start >= val.x && go_select_end <= val.x) {
+                data_select.push(val.y);
+            }
         });
         var GAL = Math.max(...data_select) / 1000;
         data_select = null;
-        var gal_rate = (GAL / data.sampleRate).toFixed(3);
 
-        //var judul = id;
-        var info_pga = GAL + ' (' + gal_rate + ')';
-        //var collection = [];
+        var info_pga = 'NO DATA';
+        if (GAL >= 0) {
+            var gal_rate = (GAL / data.sampleRate).toFixed(3);
+            info_pga = GAL + ' (' + gal_rate + ')';            
+            /*
+            if(data.id == "GE.JAGI..BHZ"){
+                var nilai = 700+(GAL * 10);
+                beepme(50,nilai,TMP_Sampel_Rate);
+                console.log("beep: ",nilai);
+            } 
+            console.log('ada data');
+            */
 
-        if (gal_rate >= 0.200) {
-            console.log('ada gempa');
-        } else {
-            console.log('tidak ada gempa');
+            if (gal_rate >= 0.200) {
+                console.log('ada gempa');
+                beepme(50,nilai,TMP_Sampel_Rate);
+            } else {
+                //console.log('tidak ada gempa');
+            }            
         }
 
         //jika tmp sampel sudah di update, update it ke gui
@@ -191,7 +202,7 @@ function sync() {
                 ('\
                  Time: ' + time + ' LocalTime <br>\
                  PGA: ' + info_pga + ' <br>\
-                 Delayed: ' + delayed + ' sec (<time data-now="' + data.end + '" data-type="sec"></time>) <br> \
+                 Delayed: ' + delayed + ' sec <br> \
                 ');
 
             var tb = data.chart;
@@ -230,43 +241,61 @@ function sync() {
                     dataLabels: {
                         enabled: false
                     },
-                    stroke: {
-                        curve: 'smooth'
-                    },
                     title: {
                         text: 'RAW DATA',
                         align: 'left'
                     },
-                    markers: {
-                        size: 0
-                    },
                     xaxis: {
                         type: 'numeric',
-                    },
-                    legend: {
-                        show: false
-                    },
+                    }
                 });
                 chart.render();
+
+                //pust chart
                 station[sta].chart = chart;
+                tb = chart;
 
             } else {
-
-                //jika chart sudah ada
                 tb.updateSeries([{
                     data: sampel_tmp
                 }]);
-
-                // update body
-                document.getElementById(data.id).querySelector('#subbody').innerHTML = infobody;
             }
+
+            //input chart here
+            if (tb !== null) {
+                tb.clearAnnotations();
+                tb.addXaxisAnnotation({
+                    x: Math.floor(timenow / TMP_Sampel_Rate),
+                    strokeDashArray: 0,
+                    borderColor: "#775DD0",
+                    label: {
+                        borderColor: "#775DD0",
+                        style: {
+                            color: "#fff",
+                            background: "#775DD0"
+                        },
+                        text: "Time Now"
+                    }
+                });
+                tb.addXaxisAnnotation({
+                    x: go_select_start,
+                    x2: go_select_end,
+                    fillColor: '#B3F7CA',
+                    label: {
+                        text: 'Analyze'
+                    }
+                });
+            }
+
+            // update body
+            document.getElementById(data.id).querySelector('#subbody').innerHTML = infobody;
         }
 
     };
 
     sampel_tmp = null;
 }
-setInterval(sync, 1000 * sync_data);
+setInterval(sync, TMP_Sampel_Rate * delayed_sync_data);
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
--- sync.sh
+++ sync.sh
@@ -169,6 +169,7 @@ mkdir cdn && cd cdn || exit
 wget https://markknol.github.io/console-log-viewer/console-log-viewer.js
 wget https://raw.githubusercontent.com/joewalnes/smoothie/master/smoothie.js
 wget https://github.com/dorukeker/gyronorm.js/raw/master/dist/gyronorm.complete.min.js
+wget https://github.com/joewalnes/reconnecting-websocket/raw/master/reconnecting-websocket.min.js
 # wget https://s.ytimg.com/yts/jsbin/www-widgetapi-vflh3Z-Yc/www-widgetapi.js -O youtube.js
 
 cp * $patch
